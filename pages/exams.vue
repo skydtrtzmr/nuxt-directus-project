5e4b51c38ced49d9524bb6e639c4e160404cae6a
skydtrtzmr <2368178395@qq.com>
2024-12-04 22:37:31 +0800

    完全替换以前的exams

--- pages/exams.vue
+++ pages/exams.vue
@@ -1,86 +1,231 @@
-<!-- Path -->
 <template>
-    <!-- <div>{{ submitted_exams }}</div> -->
-    <div>
-        <h2>考试列表</h2>
-        <div class="exam-card-container">
-            <Dialog
-                v-model:visible="not_started_dialog_visible"
-                modal
-                header="提示"
-                :style="{ width: '25rem' }"
+    <div class="card">
+        <Dialog
+            v-model:visible="not_started_dialog_visible"
+            modal
+            header="提示"
+            :style="{ width: '25rem' }"
+        >
+            <span class="text-surface-500 dark:text-surface-400 block mb-8"
+                >未到考试开始时间！</span
             >
-                <span class="text-surface-500 dark:text-surface-400 block mb-8"
-                    >未到考试开始时间！</span
-                >
-                <div class="flex justify-end gap-2">
-                    <Button
-                        type="button"
-                        label="确定"
-                        @click="not_started_dialog_visible = false"
-                    ></Button>
-                </div>
-            </Dialog>
-            <Dialog
-                v-model:visible="have_ended_dialog_visible"
-                modal
-                header="提示"
-                :style="{ width: '25rem' }"
-            >
-                <span class="text-surface-500 dark:text-surface-400 block mb-8"
-                    >已过考试结束时间！</span
-                >
-                <div class="flex justify-end gap-2">
-                    <Button
-                        type="button"
-                        label="确定"
-                        @click="have_ended_dialog_visible = false"
-                    ></Button>
-                </div>
-            </Dialog>
-            <div
-                class="exam-card"
-                v-for="submitted_exam in submitted_exams"
-                :key="submitted_exam.id"
-            >
-                <div v-if="typeof submitted_exam.exam === 'object'">
-                    <h3 class="exam-name">
-                        {{ submitted_exam.exam.title }}
-                    </h3>
-                    <p>
-                        <strong>开始时间:</strong>
-                        {{
-                            dayjs(submitted_exam.exam.start_time).format(
-                                "YYYY-MM-DD HH:mm:ss"
-                            )
-                        }}
-                    </p>
-                    <p>
-                        <strong>结束时间:</strong>
-                        {{
-                            dayjs(submitted_exam.exam.end_time).format(
-                                "YYYY-MM-DD HH:mm:ss"
-                            )
-                        }}
-                    </p>
-                    <Tag
-                        :value="getSubmitStatusName(submitted_exam)"
-                        :severity="getSubmitStatus(submitted_exam)"
-                    ></Tag>
-                    <!-- 这里需要定义type，现在有报错 -->
-                    <Button
-                        @click="joinExam(submitted_exam.id)"
-                        class="join-button"
-                        :disabled="
-                            getSubmitStatusName(submitted_exam) == '已交卷'
-                        "
-                    >
-                        {{ getSubmitStatusAction(submitted_exam) }}
-                    </Button>
-                </div>
-                <br />
+            <div class="flex justify-end gap-2">
+                <Button
+                    type="button"
+                    label="确定"
+                    @click="not_started_dialog_visible = false"
+                ></Button>
             </div>
-        </div>
+        </Dialog>
+        <Dialog
+            v-model:visible="have_ended_dialog_visible"
+            modal
+            header="提示"
+            :style="{ width: '25rem' }"
+        >
+            <span class="text-surface-500 dark:text-surface-400 block mb-8"
+                >已过考试结束时间！</span
+            >
+            <div class="flex justify-end gap-2">
+                <Button
+                    type="button"
+                    label="确定"
+                    @click="have_ended_dialog_visible = false"
+                ></Button>
+            </div>
+        </Dialog>
+        <DataView
+            :value="submitted_exams"
+            :layout="layout"
+            dataKey="submitted_exams.id"
+        >
+            <template #header>
+                <div class="flex justify-end">
+                    <SelectButton
+                        v-model="layout"
+                        :options="options"
+                        :allowEmpty="false"
+                    >
+                        <template #option="{ option }">
+                            <i
+                                :class="[
+                                    option === 'list'
+                                        ? 'pi pi-bars'
+                                        : 'pi pi-table',
+                                ]"
+                            />
+                        </template>
+                    </SelectButton>
+                </div>
+            </template>
+            <template #list="slotProps">
+                <div class="flex flex-col">
+                    <div v-for="(item, index) in slotProps.items" :key="index">
+                        <div
+                            class="flex flex-col sm:flex-row sm:items-center p-6 gap-4"
+                            :class="{
+                                'border-t border-surface-200 dark:border-surface-700':
+                                    index !== 0,
+                            }"
+                        >
+                            <div class="md:w-40 relative">
+                                <Tag
+                                    :value="getSubmitStatusName(item)"
+                                    :severity="getSubmitStatus(item)"
+                                ></Tag>
+                            </div>
+                            <div
+                                class="flex flex-col md:flex-row justify-between md:items-center flex-1 gap-6"
+                            >
+                                <div
+                                    class="flex flex-row md:flex-col justify-between items-start gap-2"
+                                >
+                                    <div>
+                                        <!-- <span
+                                            class="font-medium text-surface-500 dark:text-surface-400 text-sm"
+                                            >{{ item.category }}</span
+                                        > -->
+                                        <div class="text-xl font-medium mt-2">
+                                            {{ item.exam.title }}
+                                        </div>
+                                    </div>
+                                    <div
+                                        class="bg-surface-100 p-1"
+                                        style="border-radius: 10px"
+                                    >
+                                        <div
+                                            class="bg-surface-0 flex items-center gap-2 justify-center py-1 px-2"
+                                            style="
+                                                border-radius: 10px;
+                                                box-shadow: 0px 1px 2px 0px
+                                                        rgba(0, 0, 0, 0.04),
+                                                    0px 1px 2px 0px
+                                                        rgba(0, 0, 0, 0.06);
+                                            "
+                                        >
+                                            <span>
+                                                <strong>开始时间:</strong>
+                                                {{
+                                                    dayjs(
+                                                        item.exam.start_time
+                                                    ).format(
+                                                        "YYYY-MM-DD HH:mm:ss"
+                                                    )
+                                                }}
+                                            </span>
+                                            <span>
+                                                <strong>结束时间:</strong>
+                                                {{
+                                                    dayjs(
+                                                        item.exam.end_time
+                                                    ).format(
+                                                        "YYYY-MM-DD HH:mm:ss"
+                                                    )
+                                                }}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="flex flex-col md:items-end gap-8">
+                                    <span class="text-xl font-semibold"
+                                        >考试时长{{ item.price }}</span
+                                    >
+                                    <div
+                                        class="flex flex-row-reverse md:flex-row gap-2"
+                                    >
+                                        <Button
+                                            icon="pi pi-info-circle"
+                                            outlined
+                                        ></Button>
+                                        <Button
+                                            @click="joinExam(item.id)"
+                                            class="join-button flex-auto md:flex-initial whitespace-nowrap"
+                                            :disabled="
+                                                getSubmitStatusName(item) ==
+                                                '已交卷'
+                                            "
+                                        >
+                                            {{ getSubmitStatusAction(item) }}
+                                        </Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
+
+            <template #grid="slotProps">
+                <div class="grid grid-cols-12 gap-4">
+                    <!-- 总共分为12列 -->
+                    <div
+                        v-for="(item, index) in slotProps.items"
+                        :key="index"
+                        class="col-span-12 md:col-span-6 lg:col-span-4 xl:col-span-3 p-2"
+                    >
+                        <!-- 根据屏幕尺寸，决定每行显示的数量 -->
+                        <!-- 尺寸从大到小分别是：sm/md/lg/xl/2xl -->
+                        <div
+                            class="p-6 border border-surface-200 dark:border-surface-700 bg-surface-0 dark:bg-surface-900 rounded flex flex-col"
+                        >
+                            <div class="pt-6">
+                                <div
+                                    class="flex flex-row justify-between items-start gap-2"
+                                >
+                                    <div class="text-xl font-medium mt-1">
+                                        {{ item.exam.title }}
+                                    </div>
+                                    <!-- Tag设为shrink-0，不许它被压缩！ -->
+                                    <Tag
+                                        class="shrink-0"
+                                        :value="getSubmitStatusName(item)"
+                                        :severity="getSubmitStatus(item)"
+                                    ></Tag>
+                                </div>
+                                <div class="flex flex-col gap-2 mt-6">
+                                    <div>
+                                        <strong>开始时间:</strong>
+                                        <div>
+                                            {{
+                                                dayjs(
+                                                    item.exam.start_time
+                                                ).format("YYYY-MM-DD HH:mm:ss")
+                                            }}
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <strong>结束时间:</strong>
+                                        <div>
+                                            {{
+                                                dayjs(
+                                                    item.exam.end_time
+                                                ).format("YYYY-MM-DD HH:mm:ss")
+                                            }}
+                                        </div>
+                                    </div>
+                                    <div class="flex gap-2">
+                                        <Button
+                                            @click="joinExam(item.id)"
+                                            class="join-button flex-auto whitespace-nowrap"
+                                            :disabled="
+                                                getSubmitStatusName(item) ==
+                                                '已交卷'
+                                            "
+                                        >
+                                            {{ getSubmitStatusAction(item) }}
+                                        </Button>
+                                        <Button
+                                            icon="pi pi-info-circle"
+                                            outlined
+                                        ></Button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
+        </DataView>
     </div>
 </template>
 
@@ -92,6 +237,8 @@ import type {
     SubmittedPapers,
     Exams,
 } from "~~/types/directus_types";
+import type { HintedString } from "@primevue/core";
+
 const auth = useAuth();
 const current_user = auth.user; // 获取当前用户
 console.log("current_user:\n", current_user);
@@ -110,19 +257,6 @@ const { getItems, updateItem } = useDirectusItems();
 definePageMeta({
     middleware: ["auth"],
 });
-
-const updateSubmitStatus = async (submitted_exam: SubmittedExams) => {
-    try {
-        const newItem = { submit_status: "doing" };
-        await updateItem<SubmittedExams>({
-            collection: "submitted_exams",
-            id: submitted_exam.id,
-            item: newItem,
-        });
-    } catch (e) {}
-};
-
-// const filters = { content: "testcontent", title: "Test1" };
 
 const submitted_exams = await getItems<SubmittedExams>({
     collection: "submitted_exams",
@@ -147,6 +281,20 @@ const submitted_exams = await getItems<SubmittedExams>({
     },
 });
 
+const layout = ref<"grid" | "list" | undefined>("grid"); // 默认显示为网格
+const options = ref(["list", "grid"]);
+
+const updateSubmitStatus = async (submitted_exam: SubmittedExams) => {
+    try {
+        const newItem = { submit_status: "doing" };
+        await updateItem<SubmittedExams>({
+            collection: "submitted_exams",
+            id: submitted_exam.id,
+            item: newItem,
+        });
+    } catch (e) {}
+};
+
 const submitActualStartTime = async (submitted_exam: SubmittedExams) => {
     try {
         let nowData = dayjs();
@@ -167,21 +315,26 @@ const joinExam = (examId: string) => {
 
     const exam_info = submitted_exams.find((item) => item.id === examId)!;
 
+    // 注意因为exam可能是字符串或对象，要用“as”来断言类型
     console.log("考试开始时间：");
-    const exam_start_time = dayjs(exam_info.exam.start_time);
-    console.log(dayjs(exam_info.exam.start_time));
+    const exam_start_time = dayjs((exam_info.exam as Exams).start_time);
+    console.log(dayjs((exam_info.exam as Exams).start_time));
 
     console.log("考试结束时间：");
-    const exam_end_time = dayjs(exam_info.exam.end_time);
-    console.log(dayjs(exam_info.exam.end_time));
+    const exam_end_time = dayjs((exam_info.exam as Exams).end_time);
+    console.log(dayjs((exam_info.exam as Exams).end_time));
 
     if (now_time.isBefore(exam_start_time)) {
         not_started_dialog_visible.value = true;
+        console.log("未到考试开始时间！");
+
         return;
     }
 
     if (now_time.isAfter(exam_end_time)) {
         have_ended_dialog_visible.value = true;
+        console.log("已过考试结束时间！");
+
         return;
     }
 
@@ -205,7 +358,13 @@ const joinExam = (examId: string) => {
     // 跳转到具体的考试页面，页面path的最后一项就是submitted_exams的id。
 };
 
-const getSubmitStatus = (submitted_exam: SubmittedExams) => {
+const getSubmitStatus = (
+    submitted_exam: SubmittedExams
+):
+    | HintedString<
+          "secondary" | "success" | "info" | "warn" | "danger" | "contrast"
+      >
+    | undefined => {
     switch (submitted_exam.submit_status) {
         case "done":
             return "success";
@@ -217,7 +376,7 @@ const getSubmitStatus = (submitted_exam: SubmittedExams) => {
             return "danger";
 
         default:
-            return null;
+            return "info";
     }
 };
 
@@ -230,7 +389,7 @@ const getSubmitStatusName = (submitted_exam: SubmittedExams) => {
             return "答题中";
 
         case "todo":
-            return "未开始";
+            return "未答题";
 
         default:
             return null;
@@ -252,6 +411,4 @@ const getSubmitStatusAction = (submitted_exam: SubmittedExams) => {
             return null;
     }
 };
-
-// console.log(submitted_exams);
 </script>
