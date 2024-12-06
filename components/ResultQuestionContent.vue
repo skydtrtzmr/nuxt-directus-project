14b18a3914647259f95f9e441fa8ac60cfe48226
skydtrtzmr <2368178395@qq.com>
2024-12-06 10:37:37 +0800

    开始添加result页面

---
+++ components/ResultQuestionContent.vue
@@ -0,0 +1,29 @@
+<!-- components/ResultQuestionContent.vue -->
+<!-- 在这里进行题型的判断，根据题型渲染不同的组件 -->
+<template>
+    <div v-if="selectedSubmittedQuestion.question_type === 'q_mc_single'">
+        <QMcSingle :selectedSubmittedQuestion="selectedSubmittedQuestion" />
+    </div>
+    <div v-if="selectedSubmittedQuestion.question_type === 'q_mc_multi'">
+        <QMcMulti :selectedSubmittedQuestion="selectedSubmittedQuestion" />
+    </div>
+    <div v-if="selectedSubmittedQuestion.question_type === 'q_mc_binary'">
+        <QMcBinary :selectedSubmittedQuestion="selectedSubmittedQuestion" />
+    </div>
+    <div v-if="selectedSubmittedQuestion.question_type === 'q_mc_flexible'">
+        <QMcFlexible :selectedSubmittedQuestion="selectedSubmittedQuestion" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { SubmittedQuestions } from "~/types/directus_types.js";
+import QMcSingle from "~/components/question_type/QMcSingle.vue";
+import QMcMulti from "./question_type/QMcMulti.vue";
+import QMcBinary from "./question_type/QMcBinary.vue";
+import QMcFlexible from "./question_type/QMcFlexible.vue";
+const props = defineProps<{
+    selectedSubmittedQuestion: SubmittedQuestions;
+}>();
+</script>
+
+<style scoped></style>
---
+++ components/ResultQuestionList.vue
@@ -0,0 +1,143 @@
+<!-- components/ResultQuestionList.vue -->
+<template>
+    <div class="sidebar" v-if="submittedPaperChapters.length > 0">
+        <h5>题目列表</h5>
+        <ul>
+            <li v-for="chapter in submittedPaperChapters" :key="chapter.id">
+                <div
+                    v-if="
+                        chapter.source_paper_prototype_chapter &&
+                        typeof chapter.source_paper_prototype_chapter ===
+                            'object'
+                    "
+                >
+                    {{ chapter.source_paper_prototype_chapter.title }}
+                </div>
+                <h4>{{ chapter.title }}</h4>
+                <ul>
+                    <!-- 1 章节下的题目列表，列表式 -->
+                    <!-- <li
+                            v-for="question in chapter.submitted_questions"
+                            :key="question.id"
+                        >
+                            <button @click="selectQuestion(question)">
+                                {{ question.sort_in_chapter }}
+                            </button>
+                        </li> -->
+                    <!-- 2 章节下的题目列表，卡片式 -->
+                    <div class="question-card-container">
+                        <Button
+                            v-for="question in chapter.submitted_questions"
+                            :key="question.id"
+                            :severity="getQuestionSeverity(question)"
+                            class="question-card"
+                            :class="{
+                                selected:
+                                    selectedSubmittedQuestion &&
+                                    selectedSubmittedQuestion.id ===
+                                        question.id,
+                            }"
+                            @click="handleQuestionClick(question)"
+                        >
+                            {{ question.sort_in_chapter }}
+                        </Button>
+                    </div>
+                </ul>
+                <br />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import type {
+    SubmittedPaperChapters,
+    SubmittedQuestions,
+} from "~~/types/directus_types";
+
+const props = defineProps<{
+    submittedPaperChapters: SubmittedPaperChapters[];
+    selectQuestion: (question: SubmittedQuestions) => void;
+}>();
+
+const selectedSubmittedQuestion = ref<SubmittedQuestions | null>(
+    // props.submittedPaperChapters[0].submitted_questions[0]
+    null
+);
+
+// 当前选中题目
+// console.log(
+//     "props.submittedPaperChapters[0].submitted_questions[0]",
+//     props.submittedPaperChapters[0].submitted_questions[0]
+// );
+
+const handleQuestionClick = (question: SubmittedQuestions) => {
+    console.log("handleQuestionClick", question);
+    selectedSubmittedQuestion.value = question; // 更新选中题目
+    props.selectQuestion(question); // 调用父组件传递的选择方法
+};
+
+const getQuestionSeverity = (question: SubmittedQuestions) => {
+    if (
+        question.submitted_ans_q_mc_single ||
+        (question.submitted_ans_q_mc_multi &&
+            (question.submitted_ans_q_mc_multi as any[]).length > 0) ||
+            // 自动生成的directus type中，这里是unknown，所以需要用类型断言`as any[]`。
+        question.submitted_ans_q_mc_binary ||
+        (question.submitted_ans_q_mc_flexible &&
+            (question.submitted_ans_q_mc_flexible as any[]).length > 0)
+        // 多选题、不定项选择题必须选了选项（数组长度大于0）才算做已提交；
+        // 因为这种题做完之后对应的submitted_ans就会从null变成[]或[null]，所以要加上这个判断
+    ) {
+        return "primary";
+    } else {
+        return "secondary";
+    }
+};
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    padding-inline-start: 10px;
+    /* 不要缩进太多！ */
+}
+
+.question-card-container {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr); /* 5列布局 */
+    gap: 10px; /* 卡片间距 */
+    margin-top: 10px;
+    margin-right: 10px;
+    padding-left: 0%;
+}
+
+/* .question-card {
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    padding: 10px;
+    text-align: center;
+    cursor: pointer;
+    font-size: 1rem;
+    transition: background-color 0.2s;
+} */
+
+.question-card.selected {
+    /* background-color: #28a745; 选中后的背景色 */
+    box-shadow: inset 0 0 2px 3px #747474; /* 内边框效果 */
+    font-weight: bold; /* 字体加粗 */
+}
+
+/* .accordion { */
+/* 样式设置 */
+/* } */
+/* .accordion-header { */
+/* 样式设置 */
+/* } */
+/* .accordion-body { */
+/* 样式设置 */
+/* } */
+</style>
