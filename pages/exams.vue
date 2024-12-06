51190a39950b0cf3b8f9fe2bda041583f3e2e57a
skydtrtzmr <2368178395@qq.com>
2024-12-06 23:57:04 +0800

    Update exams.vue

--- pages/exams.vue
+++ pages/exams.vue
@@ -241,6 +241,9 @@ import type {
 } from "~~/types/directus_types";
 import type { HintedString } from "@primevue/core";
 
+// 如果使用 useRouter，需要引入并使用
+const router = useRouter();
+
 const gridItems = ref([]);
 console.log("gridItems.value");
 console.log(gridItems.value); // 在这里的时候gridItems还是空数组
@@ -250,7 +253,6 @@ const current_user = auth.user; // 获取当前用户
 console.log("current_user:\n", current_user);
 
 if (!current_user) {
-    const router = useRouter();
     router.push("/auth/login");
 }
 
@@ -363,8 +365,7 @@ const joinExam = (examId: string) => {
 
     // 你可以根据examId跳转到具体的考试页面
     // 这里的 router.push 必须是 this.$router.push 或者使用 composable useRouter()
-    // 如果使用 useRouter，需要引入并使用
-    const router = useRouter();
+
     router.push(`/exam/${examId}`);
     // 跳转到具体的考试页面，页面path的最后一项就是submitted_exams的id。
 };
