a69b3fcde99f845aae762e39d3ee7260a20404c6
skydtrtzmr <2368178395@qq.com>
2025-01-10 14:47:04 +0800

    Update scheduledUpdateCache.ts

--- server/cron/scheduledUpdateCache.ts
+++ server/cron/scheduledUpdateCache.ts
@@ -1,7 +1,13 @@
 // server/cron/scheduledUpdateCache.ts
+
+// 定期更新缓存
+
 import { defineCronHandler } from "#nuxt/cron";
 import { updateCache } from "~~/server/utils/redisUtils";
 import { createDirectus, rest, readUsers, readItems } from "@directus/sdk";
+
+// TODO 目前为了方便开发，在directus中把所有权限都开放了，所以现在发起请求的时候不需要带token。
+// 后续需要把权限控制好，只允许有权限的用户访问。
 
 const {
     public: {
@@ -14,17 +20,76 @@ const directus_url = url || "http://127.0.0.1:8056";
 
 const directus_client = createDirectus(directus_url).with(rest());
 
+const concat_question_list = async () => {
+    const question_list1 = await directus_client.request(
+        readItems("questions", {
+            fields: [
+                "id",
+                "q_mc_single.*",
+                "q_mc_multi.*",
+                "q_mc_binary.*",
+                "q_mc_flexible.*",
+                "question_group.*",
+            ],
+            page: 1,
+        })
+    );
+    const question_list2 = await directus_client.request(
+        readItems("questions", {
+            fields: [
+                "id",
+                "q_mc_single.*",
+                "q_mc_multi.*",
+                "q_mc_binary.*",
+                "q_mc_flexible.*",
+                "question_group.*",
+            ],
+            page: 2,
+        })
+    );
+    const result = question_list1.concat(question_list2);
+    return result;
+};
+
+// 注意！每次请求只能返回100条数据，所以如果题目数量多于100，需要分批请求。
+
 export default defineCronHandler("everyThirtyMinutes", () => {
     // do action
     console.log("Scheduled Update Cache");
     updateCache(
-        "chapters",
+        "paper_prototype_chapters",
         () =>
             directus_client.request(
                 readItems("paper_prototype_chapters", {
                     fields: ["id", "title", "description"],
                 })
             ),
+        3600 // 1 hour
+    );
+    // TODO 暂时题目比较少，所以可以一次性把所有数据都存入redis缓存，
+    // 后续如果题目多了，必须只把热点数据存入redis缓存。
+
+    // 如果直接这样写，最多只能返回100条数据，所以需要分批请求。
+    // updateCache(
+    //     "questions",
+    //     () =>
+    //         directus_client.request(
+    //             readItems("questions", {
+    //                 fields: [
+    //                     "id",
+    //                     "q_mc_single.*",
+    //                     "q_mc_multi.*",
+    //                     "q_mc_binary.*",
+    //                     "q_mc_flexible.*",
+    //                     "question_group.*",
+    //                 ],
+    //             })
+    //         ),
+    //     3600 // 1 hour
+    // );
+    updateCache(
+        "questions",
+        () => concat_question_list(),
         3600 // 1 hour
     );
 });
