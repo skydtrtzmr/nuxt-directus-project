13c635ce69f0a445599ca70be53f1d7fa370f76c
skydtrtzmr <2368178395@qq.com>
2025-01-21 21:53:23 +0800

    Update [...path].vue

--- pages/sections/[...path].vue
+++ pages/sections/[...path].vue
@@ -22,20 +22,18 @@ let path = route.fullPath;
 
 path = path.replace("/sections", "");
 
-const fetchSubmittedExam = async () => {
+const fetchSections = async () => {
     let currentContent = "";
     let parentId: string | null = null; // 初始先从没有parent的最上层开始
     for (let pathItem of pathList) {
-        const filter = parentId
-            ? {
-                  title: pathItem,
-                  parent_node: parentId,
-              }
-            : { title: pathItem, parent_node: { _null: true } };
+        const filter = {
+            title: pathItem,
+            parent_node: parentId ? parentId : { _null: true },
+        };
         const sections: Sections[] = await getItems<Sections>({
             collection: "sections",
             params: {
-                fields: ["id", "title", "path", "content"], // 获取考试的状态和关联的试卷
+                fields: ["id", "title", "path", "content"],
                 filter: filter,
             },
         });
@@ -47,11 +45,11 @@ const fetchSubmittedExam = async () => {
     return currentContent;
 };
 
-const { data } = await useAsyncData('sectionContent', fetchSubmittedExam);
+const { data } = await useAsyncData("sectionContent", fetchSections);
 // 使用useAsyncData函数，使得页面加载时，先获取数据，再渲染页面
 
 onMounted(async () => {
-    // await fetchSubmittedExam(); // 如果放在这里，就是客户端渲染，数据获取会导致页面闪烁
+    // await fetchSections(); // 如果放在这里，就是客户端渲染，数据获取会导致页面闪烁
 });
 </script>
 
