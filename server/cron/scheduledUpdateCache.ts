e0cb66ccb000c3c943eadfab76d7c0ca8e66c05b
skydtrtzmr <2368178395@qq.com>
2025-06-08 00:40:53 +0800

    去除redis

--- server/cron/scheduledUpdateCache.ts
+++ server/cron/scheduledUpdateCache.ts
@@ -3,10 +3,10 @@
 // 定期更新缓存
 
 import { defineCronHandler } from "#nuxt/cron";
-import { updateHashCache, updateListCache } from "~~/server/utils/redisUtils";
-import directus_client from "~~/server/lib/directus";
-import { readUsers, readItems } from "@directus/sdk";
-import { fetchAllPaginatedData } from "../utils/directusUtils";
+// import { updateHashCache, updateListCache } from "~~/server/utils/redisUtils";
+// import directus_client from "~~/server/lib/directus";
+// import { readUsers, readItems } from "@directus/sdk";
+// import { fetchAllPaginatedData } from "../utils/directusUtils";
 
 // TODO 目前为了方便开发，在directus中把所有权限都开放了，所以现在发起请求的时候不需要带token。
 // 后续需要把权限控制好，只允许有权限的用户访问。
@@ -19,26 +19,29 @@ export default defineCronHandler("everyThirtyMinutes", async () => {
 
     console.log("准备执行返回学生list");
     // 仅测试用：返回学生用户的list
-    updateListCache(
-        "student_user_email_list",
-        async () =>
-            await directus_client.request(
-                readUsers({
-                    fields: ["email"],
-                    sort: "email",
-                    filter: {
-                        role: {
-                            name: {
-                                _eq: "学生",
-                            },
-                        },
-                    },
-                    limit: -1,
-                })
-            ),
-        "email",
-        3600
-    );
+    // updateListCache(
+    //     "student_user_email_list",
+    //     async () =>
+    //         await directus_client.request(
+    //             readUsers({
+    //                 fields: ["email"],
+    //                 sort: "email",
+    //                 filter: {
+    //                     role: {
+    //                         name: {
+    //                             _eq: "学生",
+    //                         },
+    //                     },
+    //                 },
+    //                 limit: -1,
+    //             })
+    //         ),
+    //     "email",
+    //     3600
+    // );
+
+    console.log("schedule update cache end");
+    // [2025-06-08] 目前彻底把跟redis的交互从nuxt摘除了，一切从directus走。
 });
 
 // 已经在nuxt.config.ts中设置了`runOnInit: true`，就不再在函数中写`{ runOnInit: true }`了。
